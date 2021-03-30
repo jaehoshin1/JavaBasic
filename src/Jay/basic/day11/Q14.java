6fb83379d1b004d8b4551f2774a5051e2889170e
jaehoshin1 <sjh870072@naver.com>
2021-03-30 18:20:45 +0900

    dqy11

---
+++ src/Jay/basic/day11/Q14.java
@@ -0,0 +1,14 @@
+package Jay.basic.day11;
+
+public class Q14 {
+    public static void main(String[] args) {
+        int day = 86400;
+        int hour = 3600;
+        int min = 60;
+        System.out.println(1234567890/86400+ "일");
+        System.out.println(98765/3600+ "시간");
+        System.out.println(12345/60+"분");
+    }
+}
+
+//Q25
